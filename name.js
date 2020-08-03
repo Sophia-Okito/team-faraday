61b121d963c3950d5ae5e3bcc0aa9019ccf5ed84
CtrlAltFix-Softwares <blessedmarcel1@gmail.com>
2020-08-03 13:50:34 +0100

    my first commit

--- name.js
+++ name.js
@@ -5,7 +5,7 @@ function message() {
    var biostack = "Software Development";
    var slackName = "@Clement"
    
-    var intro = "Hello world I am " + fullName + " with slack email " + email + " using " + language + " biostack " + biostack + " and slack username " + slackName + ;
+    var intro = "Hello world I am " + fullName + " with slack email " + email + " using " + language + " biostack " + biostack + " and slack username " + slackName;
    }
    
    console.log(message ())
