e6df5fd8db382faf9e4fc860ab3033f287f8594b
CtrlAltFix-Softwares <blessedmarcel1@gmail.com>
2020-08-03 19:22:28 +0100

    My first commit

---
+++ name.js
@@ -0,0 +1,14 @@
+
+
+var call="Hello, I am Azubuike Nnamdi Clement by name with slack email blessedmarcel1@gmail.com, Software Development as my Biostack and my slack name is @Clement."
+
+function HackBio(){
+
+    console.log (call);
+
+}
+
+var a="true"
+if(a="true"){
+console.log (HackBio());
+};
