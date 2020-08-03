b7f39d5622ab8ea39fbe319611bbdc63f79ea988
CtrlAltFix-Softwares <blessedmarcel1@gmail.com>
2020-08-03 13:29:01 +0100

    commit on hackbio internship

---
+++ name.js
@@ -0,0 +1,11 @@
+function message() { 
+    var fullName = "Azubuike Nnamdi Clement";
+   var email = "blessedmarcel1@gmail.com";
+   var language = "JavaScript";
+   var biostack = "Software Development";
+   var slackName = "@Clement"
+   
+    var intro = "Hello world I am " + fullName + " with slack email " + email + " using " + language + " biostack " + biostack + " and slack username " + slackName + ;
+   }
+   
+   console.log(message ())
