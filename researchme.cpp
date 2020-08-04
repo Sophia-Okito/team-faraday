c00013502e809f15680f2bf85618d6518dc21f02
Sophia-Okito <63434504+Sophia-Okito@users.noreply.github.com>
2020-08-04 16:13:17 +0100

    Update researchme.cpp

--- researchme.cpp
+++ researchme.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main() {
-    printf("Name: Awopetu Mobolaji Johnson C\nEmail: mobolaji.awopetu@uniosun.edu.ng\nLanguage: C++\nBiostack: Genomics\nSlack Username: @Researchme");
+    printf("Name: Awopetu Mobolaji Johnson \nEmail: mobolaji.awopetu@uniosun.edu.ng\nLanguage: C++\nBiostack: Genomics\nSlack Username: @Researchme");
     return 0;
 }
