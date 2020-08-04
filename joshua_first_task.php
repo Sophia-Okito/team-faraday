619970fc71c7258f8ddbaef5ddaa64d16614b6ea
joshua108-data <69132847+joshua108-data@users.noreply.github.com>
2020-08-04 20:56:08 +0100

    Add files via upload

---
+++ joshua_first_task.php
@@ -0,0 +1,12 @@
+<?php
+$fullName = "Owoyemi Joshua Oladiipo";
+$email = "joshuaoladiipo108@gmail.com";
+$lang = 'PHP';
+$bioStack = "Data analytics";
+$username = "@Joshua";
+echo "Name: "  . $fullName. "\n";
+echo "Email: " . $email, "\n";
+echo "Language: " . $lang. "\n";
+echo "Biostack: ". $bioStack. "\n";
+echo "Slack Uersame " . $username;
+?>
