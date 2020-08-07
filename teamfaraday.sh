c65ecf588685958fc39facaf969a9e40a0e30849
Sophia-Okito <63434504+Sophia-Okito@users.noreply.github.com>
2020-08-07 12:53:05 +0100

    Revert "csv file"
    This reverts commit e7ee77d9342912c89d18c057eefa2e4bf318dd5b.

--- teamfaraday.sh
+++
@@ -1,7 +0,0 @@
-​#!​/bin/bash​ URL=​"​https://github.com/Sophia-Okito/team-faraday.git"​
-git clone ​"https://github.com/Sophia-Okito/team-faraday​"
-folder=​${URL​:​32}​
-new_folder=​~​/​$folder​
-​cd​ ​"​${new_folder}​"​
-cat ​*​.txt ​>​ teamfaraday.csv
-​echo​ CSV file generated
