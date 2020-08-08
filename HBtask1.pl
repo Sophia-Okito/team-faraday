785c0cd152e9561b1b7be8b1812a5a2eff5d499d
Sophia-Okito <63434504+Sophia-Okito@users.noreply.github.com>
2020-08-08 21:07:06 +0100

    Update HBtask1.pl

--- HBtask1.pl
+++ HBtask1.pl
@@ -2,4 +2,4 @@
 
 ## a program that prints my name, email id, Language used to code, my biostack, and my slack username
 
-print ("Name: Lode Prajwala Prashamsa \nEmail id: prashamsa1998@gmail.com \nLanguage used: Perl \nBiostack: Drug Development \nSlack username: @Prashamsa"); 
+print ("Name: Lode Prajwala Prashamsa\nEmail: prashamsa1998@gmail.com\nLanguage: Perl\nBiostack: Drug Development\nSlack username: @Prashamsa"); 
