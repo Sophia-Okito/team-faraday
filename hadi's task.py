10fc60d50429808128ee8aa70db6982b65bb869f
Sophia-Okito <63434504+Sophia-Okito@users.noreply.github.com>
2020-08-08 02:07:11 +0100

    Rename hadi's task.py to hadi.py

--- hadi's task.py
+++
@@ -1,6 +0,0 @@
-name = "Hadi Skaff"
-email = "skaf_hadi9@hotmail.com"
-language= "Python"
-biostack = "Software Development"
-slack_username = "@hadi_s"
-print(("Name: {} \nEmail: {} \nLanguage: {} \nBiostack: {} \nSlack Username: {}").format(name, email, language, biostack, slack_username))
