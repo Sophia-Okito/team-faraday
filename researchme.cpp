3e220be132e01d673ed985bdb3a8f5a54c3a1fa8
Sophia-Okito <63434504+Sophia-Okito@users.noreply.github.com>
2020-08-08 01:23:48 +0100

    Delete researchme.cpp

--- researchme.cpp
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main() {
-    printf("Name: Awopetu Mobolaji Johnson \nEmail: mobolaji.awopetu@uniosun.edu.ng\nLanguage: C++\nBiostack: Genomics\nSlack Username: @Researchme");
-    return 0;
-}
