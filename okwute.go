1bd0f39b933f0189e3f2dec640c495c9382660cc
Sophia-Okito <63434504+Sophia-Okito@users.noreply.github.com>
2020-08-08 01:23:07 +0100

    Delete okwute.go

--- okwute.go
+++
@@ -1,7 +0,0 @@
-package main
-import "fmt"
-
-func main(){
-	// your code goes here
-	fmt.Printf("Name: Okwute Patrick Godwin\nEmail: patrickfavor.hp@gmail.com\nLanguage:Go\nBiostack: Genomics\nSlack Username: @Okwute")
-}
