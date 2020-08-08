bde7d9b3f3ee4f4576ff3bf933b34f2b64fd00b6
Sophia-Okito <63434504+Sophia-Okito@users.noreply.github.com>
2020-08-08 03:24:43 +0100

    Add files via upload

---
+++ Sophia.py
@@ -0,0 +1,6 @@
+name = "Sophia Okito"
+email = "okitooghenetega@gmail.com"
+language= "Python"
+biostack = "Data Analytics"
+slack_username = "@Sophia"
+print(("Name: {} \nEmail: {} \nLanguage: {} \nBiostack: {} \nSlack Username: {}").format(name, email, language, biostack, slack_username))
