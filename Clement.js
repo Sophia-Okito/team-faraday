5b619781e8fc3db81f7830b93180df883ec560c4
Sophia-Okito <63434504+Sophia-Okito@users.noreply.github.com>
2020-08-08 02:59:17 +0100

    Update and rename name.js to Clement.js

---
+++ Clement.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+var name = "Azubuike Nnamdi Clement";
+var email = "blessedmarcel1@gmail.com";
+var language = "Javascript";
+var biostack = "Software Development";
+var slack_username = "@Clement";
+
+    console.log ("Name: ", name);
+    console.log ("Email: ", email);
+    console.log ("Language: ", language);
+    console.log ("Biostack: ", biostack);
+    console.log ("Slack Username: ", slack_username);
+
